2635d117ccfcf42110641467b9fb17597080ea04
amansaini7999 <amansaini7999@gmail.com>
2020-06-27 23:57:31 +0530

    TCS

---
+++ Suggested Questions/TCS/BinarySearch.cpp
@@ -0,0 +1,45 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+int bin_search(int A[],int left,int right,int k);
+
+int main()
+{
+    int t;
+    cin>>t;
+
+    while(t--)
+    {
+        int N;
+        cin>>N;
+        int a[N];
+        for(int i=0;i<N;i++)
+            cin>>a[i];
+        int key;
+        cin>>key;
+        int found = bin_search(a,0,N-1,key);
+        cout<<found<<endl;
+    }
+}
+// } Driver Code Ends
+
+
+/*You need to complete this function */
+int bin_search(int A[], int left, int right, int k)
+{
+  while(left<=right)
+  {
+    int mid=(left+right)/2;
+
+    if(A[mid]==k)
+      return mid;
+
+    else if(A[mid]<k)
+      left=mid+1;
+
+    else
+      right=mid-1;
+  }
+  return -1;
+}
